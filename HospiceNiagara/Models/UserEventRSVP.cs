390e7645543c942458c4ad6653f7c93ed1625e0d
Paul <humbertosanchas@hotmail.com>
2015-02-04 23:23:44 -0500

    Wtf

---
+++ HospiceNiagara/Models/UserEventRSVP.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospiceNiagara.Models
+{
+    public class UserEventRSVP
+    {
+        public int ID { get; set; }
+        public virtual JobDescription JobDescriptions { get; set; }
+        public virtual IdentityUser IdentityUsers { get; set; }
+        public bool HasRSVPed { get; set; }
+    }
+}
