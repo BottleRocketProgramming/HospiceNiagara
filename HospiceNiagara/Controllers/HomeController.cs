0303caf8eca595e42fc2e73378c18ce03e1d0b60
AndreasKing <romitiger@yahoo.ca>
2015-03-12 23:07:52 -0400

    Populated Job descriptions with dummy data and got the announcments section working on the hope page

--- HospiceNiagara/Controllers/HomeController.cs
+++ HospiceNiagara/Controllers/HomeController.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using HospiceNiagara.Models;
+using HospiceNiagara.ViewModels;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity.Infrastructure;
 
 namespace HospiceNiagara.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
+            var ann = db.Announcements.Include(a => a.RolesLists);
+
+            ViewData["AnnouncementOrEvent"] = ann.ToList();
             return View();
         }
 
