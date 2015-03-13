71aa8fa8207853f4cb2390bf0cc14bb6c21623a3
AndreasKing <romitiger@yahoo.ca>
2015-03-13 00:00:41 -0400

    Merge

---
+++ HospiceNiagara/Controllers/DeathNoticeController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HospiceNiagara.Models;
+
+namespace HospiceNiagara.Controllers
+{
+    public class DeathNoticeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: DeathNotice
+        public ActionResult Index(int? id)
+        {
+            ViewData["DeathNoticeList"] = db.DeathNotices.ToList();
+
+            ViewData["DeathNoticeID"] = id;
+
+            DeathNotice deathNotice = db.DeathNotices.Find(id);
+            return View();
+        }
+
+        // GET: DeathNotice/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DeathNotice deathNotice = db.DeathNotices.Find(id);
+            if (deathNotice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deathNotice);
+        }
+
+        // GET: DeathNotice/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DeathNotice/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        [OnAction(ButtonName = "CreateDeathNotice")]
+        public ActionResult Create([Bind(Include = "ID,DnFirstName,DnMiddleName,DnLastName,DnLocation,DnNotes")] DeathNotice deathNotice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DeathNotices.Add(deathNotice);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(deathNotice);
+        }
+
+        // GET: DeathNotice/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DeathNotice deathNotice = db.DeathNotices.Find(id);
+            if (deathNotice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deathNotice);
+        }
+
+        // POST: DeathNotice/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,DnFirstName,DnMiddleName,DnLastName,DnLocation,DnNotes")] DeathNotice deathNotice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(deathNotice).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(deathNotice);
+        }
+
+        // GET: DeathNotice/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DeathNotice deathNotice = db.DeathNotices.Find(id);
+            if (deathNotice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deathNotice);
+        }
+
+        // POST: DeathNotice/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DeathNotice deathNotice = db.DeathNotices.Find(id);
+            db.DeathNotices.Remove(deathNotice);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
