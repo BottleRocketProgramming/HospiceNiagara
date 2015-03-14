b35d1d3467e60b8978478dad3cd70d0d7c5f1831
Paul <humbertosanchas@hotmail.com>
2015-03-13 22:54:02 -0400

    include all files

---
+++ HospiceNiagara/ViewModels/FileSubCatVM.cs
@@ -0,0 +1,20 @@
+﻿using HospiceNiagara.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospiceNiagara.ViewModels
+{
+    public class FileSubCatVM
+    {
+       
+        public int ID { get; set; }
+      
+        public string FileSubCatName { get; set; }
+
+        public virtual FileCat FileCatFK { get; set; }
+
+        public bool FileSubCatAssigned { get; set; }
+    }
+}
