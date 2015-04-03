c86deacaf02c1bb1a3a5886ca8e4748ea6e27937
humbertosanchas <humbertosanchas@hotmail.com>
2015-04-02 22:59:47 -0400

    remove date schd

---
+++ HospiceNiagara/Models/IdentityModels.cs
@@ -0,0 +1,73 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Collections.Generic;
+using System.Web.Security;
+
+//Paul Boyko Feb 2015
+
+namespace HospiceNiagara.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+        public string UserFName { get; set; }
+        public string UserMName { get; set; }
+        public string UserLName { get; set; }
+        public DateTime? UserDOB { get; set; }
+        public string UserAddress { get; set; }
+        public virtual ICollection<JobDescription> JobDescriptions { get; set; }
+        public virtual ICollection<Announcement> UserAnnouncements { get; set; }
+
+
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("HospiceCFEntities", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<Announcement> Announcements { get; set; }     
+        public DbSet<BoardContact> BoardContacts { get; set; }
+        public DbSet<DeathNotice> DeathNotices { get; set; }
+        public DbSet<FileStorage> FileStorages { get; set; }       
+        public DbSet<JobDescription> JobDescriptions { get; set; }
+        public DbSet<Meeting> Meetings { get; set; }      
+        public DbSet<RoleList> RoleLists { get; set; }
+        public DbSet<FileCat> FileCats { get; set; }
+        public DbSet<FileSubCat> FileSubCats { get; set; }
+        public DbSet<SchedType> SchedTypes { get; set; }
+        
+        
+        
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public System.Data.Entity.DbSet<HospiceNiagara.Models.Schedule> Schedules { get; set; }
+
+              
+    }
+}
---
+++ HospiceNiagara/Models/Schedule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+//Paul Boyko Feb 2015
+
+namespace HospiceNiagara.Models
+{
+    public class Schedule
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Schedule")]
+        [StringLength(50, ErrorMessage = "Shedule name can not exceed 50 characters")]
+        public string SchedName { get; set; }
+
+        [Required]
+        [StringLength(510, ErrorMessage = "Schedule Link URL can not exceed 510 characters")]
+        public string SchedLink { get; set; }       
+
+        public virtual SchedType SchedType { get; set; }
+
+        public virtual ICollection<RoleList> ScheduleRoles { get; set; }
+
+
+    }
+}
