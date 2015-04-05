9df56a88aa7953f871a3db881cf553fa69bb23b8
AndreasKing <romitiger@yahoo.ca>
2015-04-04 22:24:19 -0400

    Schedule Link Fix

--- HospiceNiagara/Extentions/Extentions.cs
+++ HospiceNiagara/Extentions/Extentions.cs
@@ -4,22 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+//Andreas King 2015
+
 namespace HospiceNiagara.Extentions
 {
     public static class Extentions
     {
-        public static string IsActive(this HtmlHelper html,
-                                        string control,
-                                        string action)
+        public static string IsActive(this HtmlHelper html, string control, string action)
         {
             var routeData = html.ViewContext.RouteData;
 
             var routeAction = (string)routeData.Values["action"];
             var routeControl = (string)routeData.Values["controller"];
-
-            // both must match
-            var returnActive = control == routeControl &&
-                                action == routeAction;
+            var returnActive = control == routeControl && action == routeAction;
 
             return returnActive ? "nav-selected" : "";
         }
